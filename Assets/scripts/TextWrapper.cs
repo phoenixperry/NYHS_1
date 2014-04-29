85469edf62fc06cd2f749c4c7d94b7f6e6b25d60
Ben Johnson <bkbjohnson@gmail.com>
2014-04-29 13:38:34 -0400

    TextMesh versions of text replaced GUI versions

---
+++ Assets/scripts/TextWrapper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextWrapper : MonoBehaviour {
+
+	public TextSize ts;
+	public float lineWidth = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		ts = new TextSize(gameObject.GetComponent<TextMesh>());
+		Debug.Log("Initial width of string: " + ts.width);
+		ts.FitToWidth(lineWidth);
+	}
+
+	void Awake() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetText() {
+		if(ts != null) {
+			Debug.Log("Initial width of string: " + ts.width);
+			ts.FitToWidth(lineWidth);
+		}
+	}
+}

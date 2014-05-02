5f0f4411a859f4d0a1e1b4cc9ac2daa6901da7c3
Phoenix Perry <contact@phoenixperry.com>
2014-05-02 08:54:00 -0400

    making sure to not override anything

--- Assets/scripts/DataPuller.cs
+++ Assets/scripts/DataPuller.cs
@@ -11,7 +11,7 @@ using System.Linq;
 
 public class Person
 {
-    
+
     public string active;
     public string hero;
     public string familyName;
@@ -20,7 +20,8 @@ public class Person
     public string lifespan;
     public string filename;
     public string filepath;
-    public string description; 
+    public string description;
+    public string id;
 }
 public class DataPuller : MonoBehaviour
 {
@@ -28,38 +29,47 @@ public class DataPuller : MonoBehaviour
     public List<XmlNodeList> myList;
     public IEnumerable<Person> heroes;
     List<Person> people;
-    List<Person> herosList;
-	List<Person> normalPeople; 
+    public static List<Person> herosList;
+    public static List<Person> normalPeople;
     //I set this up as a delegate so more than one function could subscribe to it if need be... 
     public delegate void SetData();
     public SetData dataItem;
-    
+
     //this one sets the data item to get 
-    static public int num=0;
+    static public int num = 0;
     //this vars only job is to act at a holder for the current node you want to pull out 
-    static public Person currentHero; 
+    static public Person currentHero;
 
-  //random nodes -- implement later 
-    ArrayList randomNums; 
+    public static List<Person> activeHeroes;
+    public static List<Person> inactiveHeroes;
+    public static int SetNumHeroPeople = 2;
 
-    void Awake()
+    public static List<Person> activeNormalPeople;
+    public static List<Person> inactiveNormalPeople;
+    public static int SetNumNormalPeople = 6;
+
+    void Start()
     {
 
+
         //set up the deligate to set a current hero. 
-        dataItem += PickHeroData; 
+        dataItem += PickHeroData;
 
         people = new List<Person>();
         //load up the data 
         data = new XmlDocument();
-		data.Load("./Assets/data/data.xml");
+        data.Load("./Assets/data/data.xml");
 
         XmlElement elm = data.DocumentElement;
         XmlNodeList nodeData = elm.ChildNodes;
 
         //this will get random nodes 
+        Debug.Log(nodeData.Count + " items in db");
 
-
-
+        inactiveNormalPeople = new List<Person>();
+        activeNormalPeople = new List<Person>();
+        activeHeroes = new List<Person>();
+        inactiveHeroes = new List<Person>();
 
 
         for (int i = 0; i < nodeData.Count; i++)
@@ -68,7 +78,7 @@ public class DataPuller : MonoBehaviour
             {
                 Person p = new Person();
                 p.familyName = nodeData[i][
-                        "FamilyName"].InnerText;
+                                           "FamilyName"].InnerText;
                 p.givenName = nodeData[i]["GivenName"].InnerText;
                 p.location = nodeData[i]["Location"].InnerText;
                 p.hero = nodeData[i]["Hero"].InnerText;
@@ -77,84 +87,197 @@ public class DataPuller : MonoBehaviour
                 p.filename = nodeData[i]["Filename"].InnerText;
                 p.filepath = nodeData[i]["File_Path"].InnerText;
                 p.description = nodeData[i]["HeroDescription"].InnerText;
+                p.id = nodeData[i]["UID"].InnerText;
                 people.Add(p);
+
             }
         }
         foreach (Person p in people)
         {
-         //  Debug.Log( p.familyName + " is in the database"); 
+            //			Debug.Log( p.familyName + " is in the database"); 
         }
         //if not hero toss in static not hero list?
 
         Debug.Log("the database has " + people.Count + " records ");
-        GetHeros();  
-		GetNormalPeople(); 
-		shuffleList(normalPeople); 
-		foreach(Person p in normalPeople)
-		{
-			Debug.Log(p.givenName + p.familyName);
-		}
+        GetHeros();
+
+        //		foreach(Person p in normalPeople)
+        //		{
+        //			Debug.Log(p.givenName + p.familyName);
+        //		}
+        GetNormalPeople();
+        SetActiveHeroes();
+        SetActiveNormalPeople();
+        Debug.Log("There are " + activeHeroes.Count() + " active heroes");
+        Debug.Log("There are " + activeNormalPeople.Count() + " active normal people");
+
+        shuffleList(activeNormalPeople);
+        shuffleList(activeHeroes);
+
+        //working remove test 
+        RemoveHeroFromActiveList(activeHeroes[0]);
+        RemoveNormalPersonFromActiveList(activeNormalPeople[0]);
+        Debug.Log("There are " + activeHeroes.Count() + " active heroes after remove");
+        Debug.Log("There are " + activeNormalPeople.Count() + " active normal people after remove");
+
+        Person pn = PullNewNormalPerson();
+        Debug.Log(pn.familyName + "the pulled normal person");
+        Person pa = PullNewHero();
+        Debug.Log(pa.familyName + "the pulled hero");
+
+        RemoveHeroFromActiveList(pa);
+        RemoveNormalPersonFromActiveList(pn);
+
+        Person pn1 = PullNewNormalPerson();
+        Debug.Log(pn1.familyName + "the pulled normal person double test");
+        Person pa1 = PullNewHero();
+        Debug.Log(pa1.familyName + "the pulled hero double test");
+
     }
+    //this function should set up the initial inactive and active hero lists  
+    public static void SetActiveHeroes()
+    {
+
+        for (int i = 0; i < SetNumHeroPeople; i++)
+        {
+            Person temp = herosList[i];
+            activeHeroes.Add(temp);
+        }
+        int numNotUsed = (int)herosList.Count() - SetNumHeroPeople;
+        Debug.Log(numNotUsed + " num of hereos not used.");
+        for (int i = herosList.Count() - numNotUsed; i < herosList.Count(); i++)
+        {
+            inactiveHeroes.Add(herosList[i]);
+        }
+        Debug.Log("there are " + inactiveHeroes.Count() + " innactive heroes.");
+    }
+
+    //this function lets you remove a hero from the active list and add them to the inactive one
+    public static void RemoveHeroFromActiveList(Person p)
+    {
+        for (int i = 0; i < activeHeroes.Count; i++)
+        {
+            if (p.id == activeHeroes[i].id)
+            {
+                inactiveHeroes.Add(p);
+                activeHeroes.RemoveAt(i);
+            }
+        }
+    }
+
+    //this function lets you get a new hero out of the innactive list and put it to the active list
+    public static Person PullNewHero()
+    {
+        activeHeroes.Add(inactiveHeroes[0]);
+        //always get the first one in the list 
+        Person temp = inactiveHeroes[0];
+        inactiveHeroes.RemoveAt(0);
+
+        return temp;
+    }
+    //sets up the inactive and active normal people lists 
+    public static void SetActiveNormalPeople()
+    {
+
+        if (SetNumNormalPeople > normalPeople.Count())
+        {
+            Debug.Log("invalid num of normal people. Please try again");
+            return;
+        }
+
+        for (int i = 0; i < SetNumNormalPeople; i++)
+        {
+            Person temp = normalPeople[i];
+            activeNormalPeople.Add(temp);
+        }
+        int numNotUsed = (int)normalPeople.Count() - SetNumNormalPeople;
+        for (int i = normalPeople.Count() - numNotUsed; i < normalPeople.Count(); i++)
+        {
+            inactiveNormalPeople.Add(normalPeople[i]);
+        }
+        Debug.Log("there are " + inactiveNormalPeople.Count() + "inactive Normal people");
+    }
+    //lets you remove a normal person from the current normal active list and put them in the normal inactive list 
+    public static void RemoveNormalPersonFromActiveList(Person p)
+    {
+        for (int i = 0; i < activeNormalPeople.Count(); i++)
+        {
+            if (p.id == activeNormalPeople[i].id)
+            {
+                inactiveNormalPeople.Add(p);
+                activeNormalPeople.RemoveAt(i);
+                Debug.Log("Person removed " + p.familyName);
+            }
+        }
+    }
+    //gets a new normal person out of the inactive list and adds them to the active list. 
+    public static Person PullNewNormalPerson()
+    {
+        activeNormalPeople.Add(inactiveHeroes[0]);
+        Person temp = inactiveNormalPeople[0];
+        inactiveNormalPeople.RemoveAt(0);
+        return temp;
+
+    }
+
 
     public void GetHeros()
     {
         heroes = from person in people
-                                 where person.hero == "yes"
-                                 select person;
+                 where person.hero == "yes"
+                 select person;
 
-        herosList = new List<Person>(); 
-        foreach(Person p in heroes)
+        herosList = new List<Person>();
+        foreach (Person p in heroes)
         {
-           herosList.Add(p); 
+            herosList.Add(p);
         }
-       
-        int num = herosList.Count; 
-        Debug.Log("there are " + num + " heroes" ); 
+
+        int num = herosList.Count;
+        Debug.Log("there are " + herosList.Count() + " heroes");
     }
 
-	public void GetNormalPeople()
-	{
-	
+    public void GetNormalPeople()
+    {
+        heroes = from person in people
+                 where person.hero == "no"
+                 select person;
 
-			heroes = from person in people
-				where person.hero == "no"
-					select person;
-			
-			normalPeople = new List<Person>(); 
-			foreach(Person p in heroes)
-			{
-				normalPeople.Add(p); 
-			}
-			
-			int num = normalPeople.Count; 
-			Debug.Log("there are " + num + " normal people" ); 
-		
+        normalPeople = new List<Person>();
+        foreach (Person p in heroes)
+        {
+            normalPeople.Add(p);
+        }
 
-	}
-	
+        int num = normalPeople.Count();
+        Debug.Log("there are " + normalPeople.Count() + " normal people");
+
+
+    }
+
     public void PickHeroData()
     {
         //to use. 
         //1 set the static number of the hero you want 
         // 2 call this function 
         // 3 get the current hero
-        currentHero = herosList[num] as Person;
+        currentHero = people[0] as Person;
     }
 
-	public void shuffleList(List<Person> l)
-	{
-		System.Random rng = new System.Random(); 
-	
-		var n = (int)(l.Count()); 
-		while( n > 1) 
-		{
-			int k  = (int)rng.Next(n); 
-			n--; 
-			Person temp = l[n]; 
-			l[n] = l[k]; 
-			l[k] = temp; 
-		}
+    public void shuffleList(List<Person> l)
+    {
+        System.Random rng = new System.Random();
+
+        var n = (int)(l.Count());
+        while (n > 1)
+        {
+            int k = (int)rng.Next(n);
+            n--;
+            Person temp = l[n];
+            l[n] = l[k];
+            l[k] = temp;
+        }
 
 
-	}
+    }
 }

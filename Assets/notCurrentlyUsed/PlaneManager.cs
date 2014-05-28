e9b444058f8a7d760ed83514da7d2d0c3f49a2e3
Phoenix Perry <contact@phoenixperry.com>
2014-05-28 16:28:05 -0400

    post client tweaks

--- Assets/notCurrentlyUsed/PlaneManager.cs
+++ Assets/notCurrentlyUsed/PlaneManager.cs
@@ -20,7 +20,7 @@ public class PlaneManager : MonoBehaviour {
 	public ArrayList bgPlanes;
 	public ArrayList fgPlanes;
 	public float foregroundZCutoff = 45.0f;
-
+	
 	public Color dimColor = Color.grey;
 	public float timeBetweenHeroes = 2.0f;
 	public float timeForFirstHero = 1.0f;
@@ -30,8 +30,8 @@ public class PlaneManager : MonoBehaviour {
 	public float removeBgDelay_Max = 10.0f;
 	public float spawnFgDelay_Min = 4.0f;
 	public float spawnFgDelay_Max = 10.0f;
-	public static int numBgPlanes = 40;
-	public static int numPlanes = 20; //refractor to be numFgPlanes
+	public static int numBgPlanes = 45;
+	public static int numPlanes = 25; //refractor to be numFgPlanes
 	public float radius = 8;
 	public float radiusX = 10;
 	public float startAngle, range;
@@ -40,15 +40,15 @@ public class PlaneManager : MonoBehaviour {
 	public Vector3 rotationRadius;
 	public float currentRotation = 0.0f;
 	private int counter = 0;
-
-//	public Person nextPerson;
-
+	
+	//	public Person nextPerson;
+	
 	private float heroTimer;
 	private GameObject heroInFocus;
 	private bool dimmedBackgroundNodes = false;
 	List<Vector3> positions; 
 	List<Vector3> vect3positions; 
-
+	
 	List<SpawnPoint> heroPositions;
 	List<SpawnPoint> normalPositions;
 	
@@ -83,7 +83,7 @@ public class PlaneManager : MonoBehaviour {
 	
 	public void FixedUpdate()
 	{
-
+		
 	}
 	
 	public void Update()
@@ -103,8 +103,8 @@ public class PlaneManager : MonoBehaviour {
 			//testAddfgPlaneData();
 		}
 		//will allow for new positions to be written while Unity is running 
-				if(Input.GetKeyDown(KeyCode.R))
-					 SaveNodes();
+		if(Input.GetKeyDown(KeyCode.R))
+			SaveNodes();
 		
 	}
 	
@@ -112,14 +112,14 @@ public class PlaneManager : MonoBehaviour {
 	{	  
 		heroPositions = new List<SpawnPoint>();
 		normalPositions = new List<SpawnPoint>();
-
+		
 		foreach(string pos in File.ReadAllLines("./Assets/data/dataPositions.txt"))
 		{
 			SpawnPoint sp = new SpawnPoint();
-
+			
 			sp.position = stripData(pos);
 			sp.occupied = false;
-
+			
 			if(sp.position.z < foregroundZCutoff) {
 				heroPositions.Add(sp);
 			} else {
@@ -129,7 +129,7 @@ public class PlaneManager : MonoBehaviour {
 		Debug.Log("Hero Positions: " + heroPositions.Count);
 		Debug.Log("Normal Positions: " + normalPositions.Count);
 	}
-
+	
 	public SpawnPoint GetValidSpawnPoint( List<SpawnPoint> pointList ) {
 		foreach (SpawnPoint sp in pointList) {
 			if (false == sp.occupied) {
@@ -139,7 +139,7 @@ public class PlaneManager : MonoBehaviour {
 		Debug.LogWarning("There were no unoccupied spawn points in the list!");
 		return null;
 	}
-
+	
 	public Vector3 stripData(string sourceString) 
 	{
 		
@@ -185,7 +185,7 @@ public class PlaneManager : MonoBehaviour {
 	{
 		StartCoroutine(hero.GetComponent<SetUpText>().moveToCenter());
 	}
-
+	
 	public void TintNonFocusedNodes() {
 		dimmedBackgroundNodes = true;
 		int i;
@@ -198,7 +198,7 @@ public class PlaneManager : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	public void UnTintNonFocusedNodes() {
 		int i;
 		for (i = 0; i < bgPlanes.Count; i++) {
@@ -248,24 +248,24 @@ public class PlaneManager : MonoBehaviour {
 			return null;
 		}
 		sp.occupied = true;
-
+		
 		Quaternion r = Quaternion.Euler(0.0f, 0.0f, 0.0f);
 		GameObject p = Instantiate(plane_, transform.position, r) as GameObject;
 		p.transform.Find("BodyTextMesh").GetComponent<SmoothAlpha>().MakeInvisible();
 		p.transform.position = sp.position;
 		p.GetComponent<SetUpText>().isHero = isHero;
 		p.GetComponent<SetUpText>().sp = sp;
-
+		
 		p.SetActive(true);
 		return p;
 	}
-
+	
 	public IEnumerator TryToSpawnBG() {
-//		float t = Random.Range(spawnBgDelay_Min, spawnBgDelay_Max);
-//		while (t > 0.0f) {
-//			t -= Time.fixedDeltaTime;
-//			yield return 0;
-//		}
+		//		float t = Random.Range(spawnBgDelay_Min, spawnBgDelay_Max);
+		//		while (t > 0.0f) {
+		//			t -= Time.fixedDeltaTime;
+		//			yield return 0;
+		//		}
 		yield return new WaitForSeconds(Random.Range(spawnBgDelay_Min, spawnBgDelay_Max));
 		Debug.Log ("Try To Spawn BG");
 		if (bgPlanes.Count < numBgPlanes && !dimmedBackgroundNodes) {
@@ -274,7 +274,7 @@ public class PlaneManager : MonoBehaviour {
 			if (p != null) {
 				bgPlanes.Add(p);
 			}
-//			addbgPanelData(p); 
+			//			addbgPanelData(p); 
 		} else {
 			Debug.LogWarning("Too many BG planes to add a new one!");
 		}
@@ -282,11 +282,11 @@ public class PlaneManager : MonoBehaviour {
 	}
 	
 	public IEnumerator TryToSpawnFG() {
-//		float t = Random.Range(spawnFgDelay_Min, spawnFgDelay_Max);
-//		while (t > 0.0f) {
-//			t -= Time.fixedDeltaTime;
-//			yield return 0;
-//		}
+		//		float t = Random.Range(spawnFgDelay_Min, spawnFgDelay_Max);
+		//		while (t > 0.0f) {
+		//			t -= Time.fixedDeltaTime;
+		//			yield return 0;
+		//		}
 		yield return new WaitForSeconds(Random.Range(spawnFgDelay_Min, spawnFgDelay_Max));
 		Debug.Log("Try To Spawn FG");
 		if (fgPlanes.Count < numPlanes && !dimmedBackgroundNodes) {
@@ -303,15 +303,15 @@ public class PlaneManager : MonoBehaviour {
 	
 	public IEnumerator TryToRemoveBG() {
 		yield return new WaitForSeconds(Random.Range(removeBgDelay_Min, removeBgDelay_Max));
-
+		
 		if (bgPlanes.Count > 0 && !dimmedBackgroundNodes) {
 			GameObject plane = bgPlanes[0] as GameObject;
 			StartCoroutine(plane.GetComponent<SetUpText>().fadeOut(plane.GetComponent<SetUpText>().fadeOutTimer));
 		}
-
+		
 		StartCoroutine(TryToRemoveBG());
 	}
-
+	
 	public void RecyclePerson( Person p ) {
 		if (p.hero == "no") {
 			DataPuller.RemoveNormalPersonFromActiveList(p);

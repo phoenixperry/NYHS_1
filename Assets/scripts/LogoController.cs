81339d85813cdd2879d51cef45c8610d1d8793fc
Ben Johnson <bkbjohnson@gmail.com>
2014-06-23 15:46:38 -0400

    Logo polygon will resize automatically
    - when a logo is loaded, the polygon it is attached to will resize
    according to the dimensions of the logo

--- Assets/scripts/LogoController.cs
+++ Assets/scripts/LogoController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class LogoController : MonoBehaviour {
 
+	protected static float pixelsToUnits = 0.00269081f;
+
 	public float fadeDuration = 2.0f;
 
 	// Use this for initialization
@@ -14,6 +16,11 @@ public class LogoController : MonoBehaviour {
 		Debug.Log ("logo done");
 		Texture2D img = new Texture2D(1024, 1024, TextureFormat.ARGB32, false);
 		www.LoadImageIntoTexture(img);
+
+		Debug.Log(img.width.ToString() + " " + img.height.ToString ());
+		Debug.Log (transform.localScale.ToString());
+		transform.localScale = new Vector3(img.width * pixelsToUnits, img.height * pixelsToUnits, 0.0f);
+		Debug.Log (transform.localScale.ToString());
 		GetComponent<Renderer>().material.SetTexture("_image", img);
 		StartCoroutine(fadeIn());
 	}
